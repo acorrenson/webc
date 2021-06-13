d4ea1c9fba1525a454b4e7c225afaddbff242cb9
Arthur Correnson <arthur.correnson@gmail.com>
2021-06-14 00:17:26 +0200

    WebIO interface

---
+++ src/webio.h
@@ -0,0 +1,46 @@
+#ifndef WEBIO_H
+#define WEBIO_H
+
+#include "buff.h"
+
+typedef Webio;
+
+/**
+ * @brief Allocate a new WebIO object
+ *
+ */
+void new_webio();
+
+/**
+ * @brief Open a WebIO interface
+ *
+ */
+void webio_open(Webio);
+
+/**
+ * @brief Write bytes through a WebIO interface (push)
+ *
+ * @return unsigned Number of effective bytes written
+ */
+unsigned webio_write(Webio, const char *);
+
+/**
+ * @brief Read bytes through a WebIO interface (pull)
+ *
+ * @return unsigned Number of effective bytes read
+ */
+unsigned webio_read(Webio, Buff, unsigned);
+
+/**
+ * @brief Close a WebIO interface
+ *
+ */
+void webio_close(Webio);
+
+/**
+ * @brief Deallocate a WebIO object and nullify its underlying pointer
+ *
+ */
+void del_webio(Webio *);
+
+#endif
