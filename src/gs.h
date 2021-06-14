30688bb8469881aab4a0da7191542f2c558ed4d5
Arthur Correnson <arthur.correnson@gmail.com>
2021-06-14 08:31:38 +0200

    Switch comments to doxygen blocks in gs.h

--- src/gs.h
+++ src/gs.h
@@ -1,22 +1,39 @@
-
 #ifndef WEBC_GS_HEADER
 #define WEBC_GS_HEADER
 
-/* Growable string. */
 struct gs_t {
   unsigned int len; /* Including the null terminator. */
   unsigned int cap;
   char *str;
 };
+
+/**
+ * @brief Growable strings
+ *
+ */
 typedef struct gs_t gs_t;
 
-/* Initializes the given uninitialized growable string to an empty string. */
+/**
+ * @brief Initializes the given uninitialized growable string to an empty
+ * string.
+ *
+ */
 void gs_init(gs_t *gs);
 
-/* Cleans up the given growable string, leaving garbage values. */
+/**
+ * @brief Cleans up the given growable string, leaving garbage values.
+ *
+ * @param gs
+ */
 void gs_cleanup(gs_t *gs);
 
-/* Appends the printf-formatted arguments to the given growable string. */
+/**
+ * @brief Appends the printf-formatted arguments to the given growable string.
+ *
+ * @param gs
+ * @param format
+ * @param ...
+ */
 void gs_append_f(gs_t *gs, const char *format, ...);
 
 /* Growable string use example:
@@ -33,17 +50,22 @@ void gs_append_f(gs_t *gs, const char *format, ...);
  * Check:
  *   assert(gs_check(&gs) == 0); */
 
-/* Checks the given growable string for memory corruptions,
+/**
+ * @brief Checks the given growable string for memory corruptions,
  * returns -1 if an inconsistency is detected,
- * returns 0 if it seems ok. */
+ * returns 0 if it seems ok.
+ *
+ * @param gs
+ * @return int
+ */
 int gs_check(const gs_t *gs);
 
 #ifndef WEBC_GS_GROWTH_ADD
-/* See the gs_append_f implementation. */
+/** @brief Fine-tuned constant (1) used ind append_f */
 #define WEBC_GS_GROWTH_ADD (2.3f)
 #endif
 #ifndef WEBC_GS_GROWTH_FACTOR
-/* See the gs_append_f implementation. */
+/** @brief Fine-tuned constant (2) used ind append_f */
 #define WEBC_GS_GROWTH_FACTOR (1.6f)
 #endif
 
